6cb62279a8ca535ced5f2688007bd216cdb1bdd9
jdi022222 <jdi022222@gmail.com>
2023-05-10 22:50:36 +0900

    fix: 게시글 예외 메시지에 ID정보 추가 (#2)

--- src/main/java/com/comibird/anonymousforum/common/exception/post/PostNotFoundException.java
+++ src/main/java/com/comibird/anonymousforum/common/exception/post/PostNotFoundException.java
@@ -1,4 +1,9 @@
 package com.comibird.anonymousforum.common.exception.post;
 
-public class PostNotFoundException extends RuntimeException{
+public class PostNotFoundException extends RuntimeException {
+
+    public PostNotFoundException(Long id) {
+        super("게시글을 찾을 수 없습니다. ID: " + id);
+    }
 }
+
