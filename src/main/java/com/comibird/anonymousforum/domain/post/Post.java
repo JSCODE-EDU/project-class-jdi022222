08f1bfa353fe0235c674e71123d9c69358cab279
jdi022222 <jdi022222@gmail.com>
2023-05-10 21:22:07 +0900

    feat: Post Entity, Repository, BaseEntity 생성 (#2)

---
+++ src/main/java/com/comibird/anonymousforum/domain/post/Post.java
@@ -0,0 +1,40 @@
+package com.comibird.anonymousforum.domain.post;
+
+import com.comibird.anonymousforum.domain.auditing.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Post extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    String title;
+
+    @Column(columnDefinition = "TEXT")
+    String content;
+
+    // 생성자에 @Builder 적용
+    @Builder
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
